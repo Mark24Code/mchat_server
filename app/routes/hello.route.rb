5f5cbe05a165f4a1b1d60c23da664a1eaebc7a6b
Mark24 <mark.zhangyoung@qq.com>
2022-08-08 18:07:58 +0800

    feat: startup api

--- app/routes/hello.route.rb
+++ app/routes/hello.route.rb
@@ -6,4 +6,16 @@ App.define_routes do
       message: 'hello world' + name
     })
   end
+
+  get '/startup' do
+    json({
+      code: 200,
+      message: "success",
+      data: {
+        timestamp: Time.now.to_i,
+        uid: "Mchat",
+        content: "Welcome to Mchat. Connect server success! :D"
+      }
+    })
+  end
 end
